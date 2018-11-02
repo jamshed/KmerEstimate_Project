8ae68d5c4d45a454e10c9b280e499dab623facbb
Jamshed Khan <mdkhan@cs.stonybrook.edu>
2018-11-02 04:21:44 -0400

    memory leak fixed apparently; comparison between memoryLeakFixUp.cpp and Temp.cpp required in detail for further cause finding

--- memoryLeakFixUp.cpp
+++ memoryLeakFixUp.cpp
@@ -43,18 +43,14 @@
 //#include "dna_test.h"
 #include "ntHashIterator.hpp"
 
-
-// New headers added
 #include<ctime>
-#include<mutex>
 #include<thread>
+#include<mutex>
 
 #define SPP_MIX_HASH 1
 #include "sparsepp/spp.h"
-
-// New macros defined
 #define THREAD_COUNT 8
-#define MAX_SEQ_LEN 1025
+#define MAX_LEN 1024
 
 using spp::sparse_hash_map;
 typedef sparse_hash_map<uint64_t, uint32_t> SMap;
@@ -76,8 +72,7 @@ unsigned trailing_zeros(uint64_t n) {
     return n ? __builtin_ctzll(n) : -1;
 }
 
-
-void printHelp()
+void print_help()
 {
 
     cout << "KmerEst [options] -f <fasta/fastq> -k <k-mer length>  -s <sample size> -o <output file>"    << endl
@@ -96,7 +91,7 @@ void parse_input(int argc, char **argv, int &k, int &maxSampleCount, int &covera
     for (int c = 1; c < argc; c++)  // parse command-line input
     {
         if(!strcmp(argv[c], "-h"))      // help prompt
-            printHelp();
+            print_help();
         else if(!strcmp(argv[c], "-k"))
         {
             k = atoi(argv[c+1]);        // k-mer length
@@ -126,72 +121,35 @@ void parse_input(int argc, char **argv, int &k, int &maxSampleCount, int &covera
 }
 
 
+mutex countLock;
+
 void process_sequence(char *s, int &th, uint64_t &no_kmers, int &count, int k, int maxSampleCount, vector<SMap> &MAP,
-                      mutex &thLock, mutex &no_kmersLock, mutex &countLock, vector<mutex> &mapLock, mutex &mapDropLock,
                       bool *threadFree, int threadNum)
 {
     uint64_t hash = 0;
 
-    ntHashIterator itr(s, 1, k);    // ntHash iterator to iterate over the read sequence and provide
-                                    // ntHash values for each of its k-mers of length n; initialized with
-                                    // the first length-n window on the sequence
+    ntHashIterator itr(s, 1, k); // ntHash iterator to iterate over the read sequence and provide
+                                                // ntHash values for each of its k-mers of length n; initialized with
+                                                // the first length-n window on the sequence
 
-    while (itr != itr.end())                            // iterate until the last k-mer window
+    while(itr != itr.end())                            // iterate until the last k-mer window
     {
-        if(count > maxSampleCount)
-            cout << "\n\n\nCluster fuck\n\n\n";
-
         hash = (*itr)[0];                               // get the ntHash value
-
-
-        // Relocate this to the main function, instead of increasing contention mutexes
-        /*
-        no_kmersLock.lock();
         ++no_kmers;                                     // one more k-mer read
-        no_kmersLock.unlock();
-        */
-
-        // can add micro-optimizations of bit operations here
         uint8_t tz = trailing_zeros(hash);              // #trailing_zeroes of this k-mer
 
-        //thLock.lock();
-        int threshold = th;
-        //thLock.unlock();
-
-        if(tz >= threshold)                             // if #trailing_zeroes is greater than or equal to threshold
+        if(tz >= th)                                    // if #trailing_zeroes is greater than or equal to threshold
         {                                               // then sample this k-mer
-
-            mapLock[tz].lock();
-            auto p = MAP[tz].find(hash), e = MAP[tz].end();
-            //mapLock[tz].unlock();
-
-            // mapLock[tz].lock();
-
-            if(p != e)     // k-mer already present in hash map
-            {
-                // mapLock[tz].lock();
-
+            if(MAP[tz].find(hash) != MAP[tz].end())     // k-mer already present in hash map
                 MAP[tz][hash] += 1;                     // increment k-mer count
-
-                mapLock[tz].unlock();
-            }
             else                                        // k-mer absent in hash map
             {
-                countLock.lock();
-
-                //mapLock[tz].lock();
+                //countLock.lock();
 
                 MAP[tz].insert(make_pair(hash, 1));     // insert k-mer into hash map
+                ++count;                                // increment #samples_present by one
 
-                mapLock[tz].unlock();
-
-                //cout << "Thread " << threadNum << "took hold of count lock" << endl;
-
-                // countLock.lock();
-                int presentCount = ++count;                                // increment #samples_present by one
-                // countLock.unlock();
-
-                // cout << count << endl;
+                //cout << "\r" << "count: " << count << flush;// << endl;
 
                 /*
                     I guess there is a potential bug present in this conditional check. The conditional should be
@@ -215,37 +173,23 @@ void process_sequence(char *s, int &th, uint64_t &no_kmers, int &count, int k, i
                         while(count == k):
                             drop hash maps and update count
                 */
-                if(count >= maxSampleCount)          // max sample count reached
+                if(count == maxSampleCount)                              // max sample count reached
                 {                                           // one hash map will be dropped now
                     cout << "Samples count reached " << count << endl;
 
-                    //mapDropLock.lock();
-                    //mapLock[th].lock();
-
                     int cnt = MAP[th].size();               // size of the hash map to be dropped
-                    SMap().swap(MAP[th]);                   // drop hash map from memory
-
-                    //mapLock[th].unlock();
 
                     cout << "Dropping a hash map of size " << cnt << endl;
 
-                    //countLock.lock();
                     count = count - cnt;                    // #samples_present after dropping corresponding hash map
-                    //countLock.unlock();
-
+                    SMap().swap(MAP[th]);                   // drop hash map from memory
                     //MAP[th].clear(); //MAP[th].resize(0);
-                    //thLock.lock();
-                    ++th;
-                    //thLock.unlock();
 
-                    // mapDropLock.unlock();
-                                              // increment threshold (s parameter)
+                    ++th;                                   // increment threshold (s parameter)
                     cout  << "New samples count: " << count << endl;
                 }
 
-                //cout << "Thread " << threadNum << "releasing hold of count lock" << endl;
-
-                countLock.unlock();
+                //countLock.unlock();
             }
         }
 
@@ -255,17 +199,11 @@ void process_sequence(char *s, int &th, uint64_t &no_kmers, int &count, int k, i
     threadFree[threadNum] = true;
 }
 
-
-/*
-    Use a stack / queue based method instead, later.
-*/
 int get_free_thread(bool *threadFree)
 {
-    int i;
+    int i = 0;
 
-    for(i = 0;; i = (i + 1) % THREAD_COUNT)
-        if(threadFree[i])
-            break;
+    for(; !threadFree[i]; i = (i + 1) % THREAD_COUNT);
 
     return i;
 }
@@ -288,7 +226,7 @@ int main(int argc, char** argv)
     parse_input(argc, argv, k, maxSampleCount, coverage, inpFile, outpFile);
 
     if (inpFile.empty()  || outpFile.empty())  // empty file(s) mentioned
-        printHelp();
+        print_help();
 
     FILE *inpFilePtr;
     kseq_t *seq;
@@ -316,24 +254,6 @@ int main(int argc, char** argv)
       exit(1);
     }
 
-    /*
-        sparse_hash_map is distinguished from other hash-map implementations by its stingy use of memory and by the
-        ability to save and restore contents to disk. On the other hand, this hash-map implementation, while still
-        efficient, is slower than other hash-map implementations.
-        FMI: http://goog-sparsehash.sourceforge.net/doc/sparse_hash_map.html
-    */
-    vector<SMap> MAP(64);           // array of hash maps for sampled k-mers
-
-    mutex thLock, no_kmersLock, countLock, mapDropLock;
-    vector<mutex> mapLock(64);
-
-    cout << "read the Sequences .. " << endl;
-
-    int th = 0;                     // sample-size adaptation parameter 's';
-                                    // (the threshold count of the trailing zeroes for hash values)
-    uint64_t total = 0;             // count of sequences read
-    uint64_t no_kmers = 0;          // count of k-mers read
-    int count = 0;                  // count of samples present in the hash maps currently
 
     /*
         The C header file kseq.h is a small library for parsing the FASTA/FASTQ format.
@@ -343,122 +263,73 @@ int main(int argc, char** argv)
     */
     seq = kseq_init(fileno(inpFilePtr));    // seq is the FASTA input parser
 
-    bool done = false;
-    thread T[THREAD_COUNT];
+    /*
+        sparse_hash_map is distinguished from other hash-map implementations by its stingy use of memory and by the
+        ability to save and restore contents to disk. On the other hand, this hash-map implementation, while still
+        efficient, is slower than other hash-map implementations.
+        FMI: http://goog-sparsehash.sourceforge.net/doc/sparse_hash_map.html
+    */
+    vector<SMap> MAP(64);           // array of hash maps for sampled k-mers
 
-    kseq_t *S[THREAD_COUNT];
+    cout << "read the Sequences .. " << endl;
+
+    int th = 0;                     // sample-size adaptation parameter 's';
+                                    // (the threshold count of the trailing zeroes for hash values)
+    uint64_t total = 0;             // count of sequences read
+    uint64_t no_kmers = 0;          // count of k-mers read
+    int count = 0;                  // count of samples present in the hash maps currently
+
+    double diskReadTime = 0;
+
+    thread thrd;
     bool threadFree[THREAD_COUNT];
-    // stack<int> freeThreads;
-    bool threadUsedOnce[THREAD_COUNT];
-    char sequences[THREAD_COUNT][MAX_SEQ_LEN + 1];
+    bool threadUsed[THREAD_COUNT];
+    thread T[THREAD_COUNT];
+    char S[THREAD_COUNT][MAX_LEN + 1];
+
 
     for(int i = 0; i < THREAD_COUNT; ++i)
     {
-        S[i] = kseq_init(fileno(inpFilePtr));
         threadFree[i] = true;
-        threadUsedOnce[i] = false;
+        threadUsed[i] = false;
     }
 
-    double ioTime = 0;
-
-    while(true)
+    while(true)   // read a sequence
     {
-        int t = get_free_thread(threadFree);
-
-        //cout << "HERE with thread:\t" << t << endl;
-        //cout << "Thread status: " << threadFree[t] << endl;
-
-        threadFree[t] = false;
-
-        kstream_t *originalStream = seq -> f;
-        seq = S[t];
-        seq -> f = originalStream;
-
-        clock_t ioStart = clock();
+        double readStart = clock();
 
         if(kseq_read(seq) < 0)
-        {
-            //cout << "\n\n\n\nBreaking Bad. All hells breaking loose at sequence " << total << "\n\n\n\n";
-            // cout << "seq : " << seq -> seq.s << endl;
             break;
-        }
 
-        clock_t ioEnd = clock();
+        double readEnd = clock();
 
-        ioTime += (double)(ioEnd - ioStart);
+        diskReadTime += readEnd - readStart;
 
-        S[t] = seq;
+        ++total;    // one more sequence read
+        //cout << "\r" << total << " processing ..." << flush;
 
-        total++;
+        int t = get_free_thread(threadFree);
 
-        //cout << "At sequence " << total << endl;
-        //cout << "sequence len = " << seq -> seq.l << endl;
+        strcpy(&S[t][0], seq -> seq.s);
+        threadFree[t] = false;
 
-        // S[t] = (char *)malloc(seq -> seq.l + 1);
-        // memcpy(S[t], seq -> seq.s, seq -> seq.l);
-        //T[t].terminate();
-        if(threadUsedOnce[t])
+        if(threadUsed[t])
             T[t].join();
-        threadUsedOnce[t] = true;
 
-        T[t] = thread(&process_sequence, S[t] -> seq.s, ref(th), ref(no_kmers), ref(count), k, maxSampleCount,
-                      ref(MAP), ref(thLock), ref(no_kmersLock), ref(countLock), ref(mapLock), ref(mapDropLock),
-                      threadFree, t);
+        threadUsed[t] = true;
 
-
-        /*
-        if(done)
-            break;
-
-        int t;
-        for(t = 0; t < THREAD_COUNT; ++t)
-        {
-            kstream_t *originalStream = seq -> f;
-            seq = S[t];
-            seq -> f = originalStream;
-
-            if(kseq_read(seq) < 0)
-            {
-                done = true;
-                break;
-            }
-
-            S[t] = seq;
-
-            //cout << "\n\nHERE inside nested loop\n\n";
-
-            total++;
-
-            //cout << "At sequence " << total << endl;
-            //cout << "sequence len = " << seq -> seq.l << endl;
-
-            // S[t] = (char *)malloc(seq -> seq.l + 1);
-            // memcpy(S[t], seq -> seq.s, seq -> seq.l);
-            T[t] = thread(&process_sequence_1, seq -> seq.s, ref(th), ref(no_kmers), ref(count), k, maxSampleCount,
-                          ref(MAP), ref(thLock), ref(no_kmersLock), ref(countLock), ref(mapLock));
-        }
-
-        for(int i = 0; i < t; ++i)
-        {
-            T[i].join();
-            // free(S[i]);
-        }
-        */
+        T[t] = thread(&process_sequence, &S[t][0], ref(th), ref(no_kmers), ref(count),
+                      k, maxSampleCount, ref(MAP), threadFree, 0);
+        //T[t].join();
+        //process_sequence(seq -> seq.s, th, no_kmers, count, k, maxSampleCount, MAP);
     }
+
 
     for(int i = 0; i < THREAD_COUNT; ++i)
         if(!threadFree[i])
             T[i].join();
 
-    /*
-    while(kseq_read(seq) >= 0)   // read a sequence
-    {
-        ++total;    // one more sequence read
-        //cout << "\r" << total << " processing ..." << flush;
-        process_sequence(seq -> seq.s, th, no_kmers, count, k, maxSampleCount, MAP,
-                         thLock, no_kmersLock, countLock, mapLock);
-    }
-    */
+
 
     cout << "th: " << th << endl;                       // final value of the sampling parameter s
     cout << "No. of sequences: " << total << endl;      // total sequences read
@@ -520,7 +391,8 @@ int main(int argc, char** argv)
 
     cout << "\n\nTime taken = " << elapsedSecs << " seconds\n" << endl;
     fprintf(outpFilePtr, "\n\nTime taken = %lf seconds\n", elapsedSecs);
-    cout << "IO time = " << ioTime / CLOCKS_PER_SEC << endl;
+
+    cout << "Disk read time " << diskReadTime / CLOCKS_PER_SEC << endl;
 
     fclose(outpFilePtr);
 
